ee539636257327293edd357bc186be706a2d73ff
dhanushvk18 <153421454+dhanushvk18@users.noreply.github.com>
2026-08-27 14:51:50 +0530

    Updates

--- _next/static/FetStJRxYL61d83bu_x7L/_buildManifest.js
+++ _next/static/FetStJRxYL61d83bu_x7L/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/App-PDD//_next/:path+",
-        "destination": "/App-PDD/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
